f4c35a43f3677747bcbbe25885d523294057435a
Rafael Dias <rafaeldias@MacBook-Air-de-Rafael.local>
2024-11-12 17:27:37 -0300

    feat: login

--- src/app/shared/components/auth/login/login.component.ts
+++ src/app/shared/components/auth/login/login.component.ts
@@ -1,27 +1,69 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { AuthService } from '../../../services/auth.service';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import {
+  faUser,
+  faLock,
+  faSignInAlt,
+  faExclamationCircle,
+  faUserCircle,
+} from '@fortawesome/free-solid-svg-icons';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  imports: [CommonModule,FormsModule,MatFormFieldModule,ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    FontAwesomeModule,
+    MatInputModule,
+  ],
 })
 export class LoginComponent {
   loginForm: FormGroup;
   errorMessage: string | null = null;
 
-  constructor(private fb: FormBuilder, private authService: AuthService) {
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private iconLibrary: FaIconLibrary
+  ) {
+    // Registro dos ícones necessários
+    this.iconLibrary.addIcons(
+      faUser,
+      faLock,
+      faSignInAlt,
+      faExclamationCircle,
+      faUserCircle
+    );
+
+    // Configuração do formulário de login
     this.loginForm = this.fb.group({
       username: ['', [Validators.required, Validators.minLength(4)]],
       password: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
+  /**
+   * Manipula o envio do formulário de login.
+   */
   onSubmit(): void {
     if (this.loginForm.valid) {
       const { username, password } = this.loginForm.value;
