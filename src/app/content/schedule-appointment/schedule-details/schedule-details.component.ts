d968ac3a185987fc751176acd9b321cbfaef7f58
Rafael Dias <rafaeldias@MacBook-Air-de-Rafael.local>
2024-11-12 16:52:11 -0300

    feat: calendario

--- src/app/content/schedule-appointment/schedule-details/schedule-details.component.ts
+++ src/app/content/schedule-appointment/schedule-details/schedule-details.component.ts
@@ -1,43 +1,65 @@
-import { CommonModule } from '@angular/common';
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Component, Input, EventEmitter, Output, OnInit } from '@angular/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
+import { MatOptionModule } from '@angular/material/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-schedule-details',
   standalone: true,
   imports: [
-    CommonModule,
     MatDatepickerModule,
     MatFormFieldModule,
     MatSelectModule,
     MatInputModule,
-    MatNativeDateModule, // Inclua isso
     MatOptionModule,
-    MatButtonModule, // Certifique-se de que os módulos necessários estão incluídos
+    MatButtonModule,
+    CommonModule,
+    FormsModule,
   ],
   templateUrl: './schedule-details.component.html',
   styleUrls: ['./schedule-details.component.scss'],
 })
-export class ScheduleDetailsComponent {
+export class ScheduleDetailsComponent implements OnInit {
   @Input() appointmentType: string | null = null; // Tipo de atendimento selecionado
   @Output() reset = new EventEmitter<void>(); // Emite evento para resetar o fluxo
 
+  selectedDate: Date | null = null; // Propriedade para armazenar a data selecionada
   professionals = [
     { name: 'Dr. João Silva', specialty: 'Clínico Geral' },
     { name: 'Dra. Maria Oliveira', specialty: 'Oftalmologista' },
-  ];
-
+  ]; // Dados mocados temporariamente
   availableTimes = ['08:00', '08:30', '09:00', '09:30']; // Horários disponíveis
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    // Lógica para buscar médicos do backend
+    // this.getProfessionals().subscribe((data) => {
+    //   this.professionals = data;
+    // });
+  }
+
+  /**
+   * Função para buscar médicos do backend.
+   * @returns Observable com a lista de médicos.
+   */
+  private getProfessionals(): Observable<any[]> {
+    const apiUrl = 'https://sua-api.com/medicos'; // Substitua pela URL real do backend
+    return this.http.get<any[]>(apiUrl);
+  }
 
   /**
    * Reseta o fluxo para a seleção inicial.
    */
   onReset(): void {
     this.reset.emit();
+    this.selectedDate = null;
   }
 }
